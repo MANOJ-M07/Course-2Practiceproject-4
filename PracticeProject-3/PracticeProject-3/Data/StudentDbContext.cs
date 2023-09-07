0233ff475d8b300811687f7bf09a33469c15e4ba
MANOJ-M07 <manojmoorthy01@gmail.com>
2023-09-07 18:30:25 +0530

    Initail commit

---
+++ PracticeProject-3/PracticeProject-3/Data/StudentDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PracticeProject_3.Models;
+
+namespace PracticeProject_3.Data
+{
+    public class StudentDbContext : DbContext
+    {
+        public StudentDbContext (DbContextOptions<StudentDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<PracticeProject_3.Models.Subject> Subject { get; set; } = default!;
+    }
+}
